b927df9957ed2cab75f2c5e50ed0cf8b94353d40
duhoslav <denisych259@yandex.ru>
2016-04-25 17:31:13 +0300

    Merge branch 'master' of https://github.com/Duhoslav/simulator
    Conflicts:
    	conf.json
    	main.py

--- main.py
+++ main.py
@@ -5,19 +5,53 @@ import subprocess
 import threading
 import sys
 import time
+import requests
+import StringIO
+import socket
 
-def cmd(programm,args,timeout=0):
+def sniffer(port):
+    try:
+        s = socket.socket( socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(0x0003))
+    except socket.error , msg:
+        print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        return None
+
+    # receive a packet
+    while True:
+        packet = s.recvfrom(port)
+        print packet
+
+def cmd(programm,args,maxsize,timeout=0):
+    #sniffer(65565)
+    with open('listurl') as f:
+        list_url=f.readlines()
+    start_traffic_activity(list_url,maxsize)
     pr=subprocess.Popen("./"+programm+args,stdout=subprocess.PIPE,shell=True)
-    print pr.stdout.read()
+    #print pr.stdout.read()
     time.sleep(timeout)
 
 def run_in_threads(opt,timeout):
     threads_count=int(opt['threads'])
     for i in xrange(threads_count):
-        thread=threading.Thread(target=cmd, args=[opt['programm'],opt['args']])
+        thread=threading.Thread(target=cmd, args=[opt['programm'],opt['args'],opt['legal_traffic_size']])
         thread.start()
         thread.join()
         time.sleep(timeout)
+
+def start_traffic_activity(url_list,maxsize):
+    for i in xrange(len(url_list)):
+        r=requests.get(url_list[i], stream=True)
+        size = 0
+        ctt = StringIO()
+
+
+        for chunk in r.iter_content(2048):
+            size += len(chunk)
+            ctt.write(chunk)
+            if size > maxsize:
+                r.close()
+                raise ValueError('Response too large')
+        content = ctt.getvalue()
 
 if __name__ == '__main__':
     if len(sys.argv)<2:
@@ -34,6 +68,7 @@ In this array you can specify several tries - one json object per attack. Type d
 
     file=open(sys.argv[1])
     d = json.load(file)
+
     for i in xrange(len(d['options'])):
         try:
             timeout = float(d['options'][i]['timeout'])
@@ -42,7 +77,7 @@ In this array you can specify several tries - one json object per attack. Type d
         if d['options'][i]['threads']:
             run_in_threads(d['options'][i],timeout/1000)
         else:
-            cmd(d['options'][i]['programm'],d['options'][i]['args'],timeout/1000)
+            cmd(d['options'][i]['programm'],d['options'][i]['args'],d['legal_traffic_size'],timeout/1000)
 
     file.close()
 
